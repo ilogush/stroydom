695c15c1d58a32d221b17c015f74c5a99121c5d8
Dan Efimov <efimov82@gmail.com>
2025-02-19 00:18:03 +0700

    add paraglide-js lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from "astro/config";
+import paraglide from "@inlang/paraglide-astro";
 
-export default defineConfig({
-  site: "http://localhost:3000",
-  publicDir: "public", // Явно указываем, что Astro должен использовать public/
+// export default defineConfig({
+//   site: "http://localhost:3000",
+//   publicDir: "public", // Явно указываем, что Astro должен использовать public/
+//   i18n: {
+//     defaultLocale: "ge", // the default locale
+//     locales: ["en", "ru"], // the locales you want to support
+//   },
+// });
+
+export default {
+  integrations: [
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+  ],
   i18n: {
-    defaultLocale: "ge", // the default locale
-    locales: ["en", "ru"], // the locales you want to support
+    defaultLocale: "ge",
+    locales: ["ge", "en", "ru"],
+    localeDetection: false,
   },
-});
+};
