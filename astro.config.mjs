21bc59a9b52e09c0f1a21dc9efa9e06d98fcec3e
Dan Efimov <efimov82@gmail.com>
2025-02-18 23:41:22 +0700

    add i18n config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'http://localhost:3000',
-  publicDir: 'public', // Явно указываем, что Astro должен использовать public/
+  site: "http://localhost:3000",
+  publicDir: "public", // Явно указываем, что Astro должен использовать public/
+  i18n: {
+    defaultLocale: "ge", // the default locale
+    locales: ["en", "ru"], // the locales you want to support
+  },
 });
