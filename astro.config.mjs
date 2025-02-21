e4d2a6f4486569121f16045280a02c9436e61b01
Dan Efimov <efimov82@gmail.com>
2025-02-21 08:54:21 +0800

    feat: add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,14 @@
-import { defineConfig } from "astro/config";
 import paraglide from "@inlang/paraglide-astro";
-
-// export default defineConfig({
-//   site: "http://localhost:3000",
-//   publicDir: "public", // Явно указываем, что Astro должен использовать public/
-//   i18n: {
-//     defaultLocale: "ge", // the default locale
-//     locales: ["en", "ru"], // the locales you want to support
-//   },
-// });
+import icon from "astro-icon";
 
 export default {
-  base: '/',
-  // output: 'server',
+  base: "/",
   integrations: [
     paraglide({
       project: "./project.inlang",
       outdir: "./src/paraglide",
     }),
+    icon(),
   ],
   i18n: {
     defaultLocale: "ge",
